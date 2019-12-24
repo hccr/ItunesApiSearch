342c41c0e81fe0872cda3af211a935587ea8635c
Harold Campuzano <harold.campuzano@safecar.cl>
2019-12-24 02:03:05 -0300

    Initial Commit, Se crea proyecto siguiendo la arquitectura Clean Swift, utilizando el template obtenido desde https://clean-swift.com/ Se crea una vista principal en el Main.storyboard que contiene un searchbar y un tableview. Se modifica el ViewController para poder manejar los eventos del searchbar y tableview. Se crea Grupo Model que contiene el modelo compartido por toda la aplicacion, Result.swift
    Se agrega Alamofire para realizar las peticiones al API y se empieza a implementar en el SearchWorker

---
+++ ItunesApiSearch/Scenes/SearchSongs/SearchTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  SearchTableViewCell.swift
+//  ItunesApiSearch
+//
+//  Created by Harold Campuzano Rivera on 23/12/19.
+//  Copyright © 2019 harold-campuzano. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nombreArtista: UILabel!
+    
+    @IBOutlet weak var nombreCancion: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ItunesApiSearch' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ItunesApiSearch
+  pod 'Alamofire', '~> 5.0.0-rc.3'
+
+  target 'ItunesApiSearchTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
